1e61b42a5bca33018002765b3b62bbf9a91fcd07
jyothisridhar <jyotishridhar7@gmail.com>
2016-02-26 14:22:13 +0530

    add bounce when infowindow is opened

--- p5 version1/js/app.js
+++ p5 version1/js/app.js
@@ -1,105 +1,111 @@
+var locations = ['hard rock cafe, bangalore', 'amoeba, church street, bangalore','matteo, church street, bangalore', 'social, church street, bangalore','chutney chang, museum road', 'bowring institue, bangalore', 'st. marks cathedral, bangalore', 'm chinnaswamy stadium, bangalore', 'high court of karnataka, bangalore', 'ulsoor lake pathway, bangalore'];
+
+var ViewModel = function() {
+  var self = this;
+  this.locationList = ko.observableArray([]);
+  locations.forEach(function(loc){
+    self.locationList.push(loc);
+  });
+};
 
 var map;
-/*
-Start here! initializeMap() is called when page is loaded.
-*/
 function initializeMap() {
+    var mapOptions = {
+      zoom: 8,
+    };
 
-  var mapOptions = {
-    zoom: 8,
-  };
+    map = new google.maps.Map(document.querySelector('#map-canvas'), mapOptions);
 
-  map = new google.maps.Map(document.querySelector('#map-canvas'), mapOptions);
+    /*
+    createMapMarker(placeData) reads Google Places search results to create map pins.
+    placeData is the object returned from search results containing information
+    about a single location.
+    */
+    function createMapMarker(placeData) {
+      var lat = placeData.geometry.location.lat();  // latitude from the place service
+      var lon = placeData.geometry.location.lng();  // longitude from the place service
+      var name = placeData.name;   // name of the place from the place service
+      var bounds = window.mapBounds;            // current boundaries of the map window
 
-  /*
-  createMapMarker(placeData) reads Google Places search results to create map pins.
-  placeData is the object returned from search results containing information
-  about a single location.
-  */
-  function createMapMarker(placeData) {
+      // marker is an object with additional data about the pin for a single location
+      var marker = new google.maps.Marker({
+        map: map,
+        position: placeData.geometry.location,
+        title: name,
+        animation: google.maps.Animation.DROP,
+      });
 
-    // The next lines save location data from the search result object to local variables
-    var lat = placeData.geometry.location.lat();  // latitude from the place service
-    var lon = placeData.geometry.location.lng();  // longitude from the place service
-    var name = placeData.name;   // name of the place from the place service
-    var bounds = window.mapBounds;            // current boundaries of the map window
+      // infoWindows are the little helper windows that open when you click
+      // or hover over a pin on a map. They usually contain more information
+      // about a location.
+      var infoWindow = new google.maps.InfoWindow({
+        content: name
+      });
 
-    // marker is an object with additional data about the pin for a single location
-    var marker = new google.maps.Marker({
-      map: map,
-      position: placeData.geometry.location,
-      title: name,
-      animation: google.maps.Animation.DROP,
-    });
+      google.maps.event.addListener(marker, 'click', function() {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        infoWindow.open(map, marker);
+        setTimeout(function() {
+          marker.setAnimation(null)
+        }, 1000);
+      });
 
-    // infoWindows are the little helper windows that open when you click
-    // or hover over a pin on a map. They usually contain more information
-    // about a location.
-    var infoWindow = new google.maps.InfoWindow({
-      content: name
-    });
-
-    google.maps.event.addListener(marker, 'click', function() {
-      infoWindow.open(map, marker);
-    });
-
-    // this is where the pin actually gets added to the map.
-    // bounds.extend() takes in a map location object
-    bounds.extend(new google.maps.LatLng(lat, lon));
-    // fit the map to the new marker
-    map.fitBounds(bounds);
-    // center the map
-    map.setCenter(bounds.getCenter());
-  }
-
-  /*
-  callback(results, status) makes sure the search returned results for a location.
-  If so, it creates a new map marker for that location.
-  */
-  function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for(var i = 0; i < results.length; i++) {
-        console.log("create marker", results);
-        createMapMarker(results[i]);
-      }
+      // this is where the pin actually gets added to the map.
+      // bounds.extend() takes in a map location object
+      bounds.extend(new google.maps.LatLng(lat, lon));
+      // fit the map to the new marker
+      map.fitBounds(bounds);
+      // center the map
+      map.setCenter(bounds.getCenter());
     }
-    else
-      console.log("place not found");
-  }
 
-  /*
-  pinPoster(locations) takes in the array of locations
-  and fires off Google place searches for each location
-  */
-  function pinPoster(locations) {
+    /*
+    callback(results, status) makes sure the search returned results for a location.
+    If so, it creates a new map marker for that location.
+    */
+    function callback(results, status) {
+      if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for(var i = 0; i < results.length; i++) {
+          console.log("create marker", results);
+          createMapMarker(results[i]);
+        }
+      }
+      else
+        console.log("place not found");
+    }
 
-    // creates a Google place search service object. PlacesService does the work of
-    // actually searching for location data.
-    var service = new google.maps.places.PlacesService(map);
+    /*
+    pinPoster(locations) takes in the array of locations
+    and fires off Google place searches for each location
+    */
+    function pinPoster(locations) {
 
-    var bangalore = new google.maps.LatLng(12.978825, 77.599719);
-    // Iterates through the array of locations, creates a search object for each location
-    //for (var place in locations) {
-      // the search request object
-      var request = {
-        location: bangalore,
-        radius: 1000,
-        types: ['book_store', 'art_gallery','beauty_salon','bar']
-      };
+      // creates a Google place search service object. PlacesService does the work of
+      // actually searching for location data.
+      var service = new google.maps.places.PlacesService(map);
 
-      // Actually searches the Google Maps API for location data and runs the callback
-      // function with the search results after each search.
-      service.nearbySearch(request, callback);
-    //}
-  }
+      var bangalore = new google.maps.LatLng(12.978825, 77.599719);
+      // Iterates through the array of locations, creates a search object for each location
+      //for (var place in locations) {
+        // the search request object
+        var request = {
+          location: bangalore,
+          radius: 800,
+          types: ['book_store', 'art_gallery','beauty_salon','bar']
+        };
 
-  // Sets the boundaries of the map based on pin locations
-  window.mapBounds = new google.maps.LatLngBounds();
+        // Actually searches the Google Maps API for location data and runs the callback
+        // function with the search results after each search.
+        service.nearbySearch(request, callback);
+      //}
+    }
 
-  // pinPoster(locations) creates pins on the map for each location in
-  // the locations array
-  pinPoster(locations);
+    // Sets the boundaries of the map based on pin locations
+    window.mapBounds = new google.maps.LatLngBounds();
 
+    // pinPoster(locations) creates pins on the map for each location in
+    // the locations array
+    pinPoster(locations);
 }
 // Calls the initializeMap() function when the page loads
 window.addEventListener('load', initializeMap);
@@ -110,16 +116,5 @@ window.addEventListener('resize', function(e) {
   //Make sure the map bounds get updated on page resize
 map.fitBounds(mapBounds);
 });
-
-var locations = ['hard rock cafe, bangalore', 'amoeba, church street, bangalore','matteo, church street, bangalore', 'social, church street, bangalore','chutney chang, museum road', 'bowring institue, bangalore', 'st. marks cathedral, bangalore', 'm chinnaswamy stadium, bangalore', 'high court of karnataka, bangalore', 'ulsoor lake pathway, bangalore'];
-
-var ViewModel = function() {
-  var self = this;
-  this.locationList = ko.observableArray([]);
-  locations.forEach(function(loc){
-    self.locationList.push(loc);
-  });
-
-};
 
 ko.applyBindings(new ViewModel());
